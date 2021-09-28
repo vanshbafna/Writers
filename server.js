fac465348ad37b5004f0d48097eb3ec2ace3b0ce
Creative Devloper(Vansh Bafna) <59309266+vanshbafna@users.noreply.github.com>
2021-09-28 17:11:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'tamannatadwal2017@gmail.com',
-        pass: 'ujzrtfolursfgshp',
+        user: 'vanshbafnajain@gmail.com',
+        pass: 'wfcgrjpdladotccn',
     },
     secure: true,
 });
@@ -44,7 +44,7 @@ app.post("/send-mail", (req, res) => {
     const to = req.body.to;
     const url = req.body.url;
     const mailData = {
-        from: "apoorv.goyal@whitehatjr.com",
+        from: "vanshbafnajain@mail.com",
         to: to,
         subject: "Join the video chat with me!",
         html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
